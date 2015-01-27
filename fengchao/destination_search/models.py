4f8686d9950bbb67f5d1ee451986a0f0ffeec082
Henry Yan <henry@kangkanghui.com>
2015-01-27 21:03:24 +0800

    Add the category table in the database

--- fengchao/destination_search/models.py
+++ fengchao/destination_search/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	class Meta:
+		db_table = 'category'
+
+	keyword = models.CharField(max_length=80)
+	weight = models.DecimalField(max_digits=20, decimal_places=10)
+	create_time = models.DateTimeField(auto_now_add=True)
+	last_update_time = models.DateTimeField(auto_now=True)
+	remark = models.TextField(blank=True)
+
+
+
+
